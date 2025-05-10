0db933079647dc671a22f08d4a80cf8be429b0cb
DenAgrippa <denis.b.56789@gmail.com>
2025-05-10 16:19:16 +0500

    Made it possible to set a start date and go to the next page for get_posts function

--- parser.py
+++ parser.py
@@ -4,23 +4,34 @@ import time
 import sqlite3
 
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36'}
-delay = 10
+delay = 3
 
 
-def get_posts(subreddits, start_date="none", end_date="none"):
+def get_posts(subreddits, start_date="none"):
     posts = []
     for subreddit in subreddits:
         subreddit_link = f"https://old.reddit.com/r/{subreddit}/new/"
-        web_content = requests.get(subreddit_link, verify=False, headers=headers)
-        web_content_soup = BeautifulSoup(web_content.text, "html.parser")
-        posts_containers = web_content_soup.find_all("a", {"data-event-action": "comments"})
-        for post in posts_containers:
-            post_link = post['href'].replace(f"https://old.reddit.com/r/{subreddit}/comments/", "").strip("/").split("/")
-            post_id, post_title = post_link
-            posts.append({'subreddit': subreddit,
-                          'post_id': post_id,
-                          'post_title': post_title})
-        time.sleep(delay)
+        while True:
+            exit_flag = False
+            web_content = requests.get(subreddit_link, verify=False, headers=headers)
+            web_content_soup = BeautifulSoup(web_content.text, "html.parser")
+            posts_containers = web_content_soup.find_all("div", class_="top-matter")
+            if len(posts_containers) < 25: 
+                exit_flag = True
+            for post in posts_containers:
+                post_id, post_title = post.find("a", {"data-event-action": "comments"})['href'].replace(f"https://old.reddit.com/r/{subreddit}/comments/", "").strip("/").split("/")
+                post_timestamp = post.find("time", class_="live-timestamp")['datetime']
+                if post_timestamp <= start_date:
+                    exit_flag = True
+                    break
+                posts.append({'subreddit': subreddit,
+                              'post_id': post_id,
+                              'post_title': post_title,
+                              'post_timestamp': post_timestamp})
+            if exit_flag == True:
+                break
+            subreddit_link = f"https://old.reddit.com/r/{subreddit}/new/?count=25&after=t3_{post_id}"
+            time.sleep(delay)
     return posts
 
 
@@ -42,14 +53,14 @@ def writre_to_db(posts, db_path):
     connection = sqlite3.connect(db_path)
     cursor = connection.cursor()
     try:
-        cursor.executemany("INSERT INTO posts (subreddit, post_id, post_title, post_text) VALUES(:subreddit, :post_id, :post_title, :post_text)", posts)
+        cursor.executemany("INSERT INTO posts (subreddit, post_id, post_title, post_timestamp, post_text) VALUES(:subreddit, :post_id, :post_title, :post_timestamp, :post_text)", posts)
     except:
-        cursor.execute('CREATE TABLE posts(subreddit, post_id, post_title, post_text)')
-        cursor.executemany("INSERT INTO posts (subreddit, post_id, post_title, post_text) VALUES(:subreddit, :post_id, :post_title, :post_text)", posts)
+        cursor.execute('CREATE TABLE posts(subreddit, post_id, post_title, post_timestamp, post_text)')
+        cursor.executemany("INSERT INTO posts (subreddit, post_id, post_title, post_timestamp, post_text) VALUES(:subreddit, :post_id, :post_title, :post_timestamp, :post_text)", posts)
     connection.commit()
 
 
 
-posts = get_posts(['ChatGPTJailbreak', 'Qwen_AI'])
+posts = get_posts(['AIJailbreak', 'ChatGPTJailbreak', 'ChatGptDAN', 'GPT_jailbreaks', 'OnlyJailbreaks', 'maximumai'], start_date="2025-04-01T00:00:01+00:00")
 post_text = get_text_from_posts(posts)
-writre_to_db(post_text, "exp/exp2.db")
+writre_to_db(post_text, "db/db1.db")
